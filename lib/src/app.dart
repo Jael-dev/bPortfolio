fb52f74bb2e3c9ab1d455402b19303215d11532d
jael-dev <jaelngaleu@gmail.com>
2023-03-08 21:40:07 +0100

    function: settings service created

---
+++ lib/src/app.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class BonasPortfolio extends ConsumerStatefulWidget {
+  const BonasPortfolio({super.key});
+
+
+
+  @override
+  ConsumerState<ConsumerStatefulWidget> createState() => _BonasPortfolioState();
+}
+
+class _BonasPortfolioState extends ConsumerState<BonasPortfolio> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
